fe1884e27703e0b9004a5e88644d67b21dac78d7
Tini Vasanth <tini.vasanth@gmail.com>
2021-03-22 20:41:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var FoodGroup, bananaImage;
+var obstaclesGroup, obstacle_img;
+
+var gameOver;
+var score=0;
+
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  
+  
+
+  bananaImage = loadImage("banana.png");
+  obstacle_img = loadImage("stone.png"); 
+  
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  ground.visible=false;
+  
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  
+  background(255);
+  
+    
+}
+
+
+  
